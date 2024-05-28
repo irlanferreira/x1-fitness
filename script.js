56aeb5f6e10f37d0357fe0db20cbec858dae524b
nacre-gui <147561892+nacre-gui@users.noreply.github.com>
2024-05-28 13:50:41 -0300

    imc
    me de papai

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 function calcularIMC() {
-    console.log()
-        var altura = parseFloat(document.getElementById("altura").value);
-var peso = parseFloat(document.getElementById("peso").value);
+    var altura = parseFloat(document.getElementById("altura").value);
+    var peso = parseFloat(document.getElementById("peso").value);
 
-if (altura&&peso){
-            var imc = peso / (altura * altura);
-var imc = imc.toFixed(2)
-document.getElementById("resultado").innerText = `Seu IMC é ${imc}`;
+    if (altura && peso) {
+        var imc = peso / (altura * altura);
+        var imc = imc.toFixed(2);
+
+        var classificacao = "";
+        if (imc < 18.5) {
+            classificacao = "Abaixo do peso";
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            classificacao = "Peso normal";
+        } else if (imc >= 25 && imc <= 29.9) {
+            classificacao = "Sobrepeso";
+        } else if (imc >= 30 && imc <= 34.9) {
+            classificacao = "Obesidade grau I";
+        } else if (imc >= 35 && imc <= 39.9) {
+            classificacao = "Obesidade grau II";
         } else {
-    document.getElementById("resultado").innerText = "preencha altura e peso.";
+            classificacao = "Obesidade grau III";
         }
+
+        document.getElementById("resultado").innerText = `Seu IMC é ${imc} (${classificacao})`;
+    } else {
+        document.getElementById("resultado").innerText = "Preencha altura e peso.";
     }
+}
